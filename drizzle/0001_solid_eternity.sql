564333e4361919da930b1d11719cc995a0036292
Abir-7 <md.tazwarul.islam.07@gmail.com>
2025-10-27 22:50:47 +0600

    update

--- drizzle/0001_solid_eternity.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "service_progress" ALTER COLUMN "extra_work_accept_status" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "service_progress" ALTER COLUMN "extra_work_accept_status" DROP NOT NULL;
--- drizzle/0004_heavy_stephen_strange.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "user_payment_data" DROP CONSTRAINT "user_payment_data_user_profile_id_unique";--> statement-breakpoint
-ALTER TABLE "user_payment_data" DROP CONSTRAINT "user_payment_data_user_profile_id_user_profiles_user_id_fk";
---> statement-breakpoint
-ALTER TABLE "user_payment_data" ADD COLUMN "user_id" uuid;--> statement-breakpoint
-ALTER TABLE "user_payment_data" ADD CONSTRAINT "user_payment_data_user_id_user_profiles_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user_profiles"("user_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "user_payment_data" DROP COLUMN "user_profile_id";--> statement-breakpoint
-ALTER TABLE "user_payment_data" ADD CONSTRAINT "user_payment_data_user_id_unique" UNIQUE("user_id");
